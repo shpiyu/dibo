ad0b7ca1dbe22cee119f18571f6ec2c72abb3f21
PrasannaC <choulinux@gmail.com>
2016-06-25 11:51:05 +0530

    Firebase upgrades

--- firebaseUtils.js
+++ firebaseUtils.js
@@ -4,15 +4,27 @@ exports = module.exports;
 
 exports.FireBase = {
 
-		
 		hello : function(){ console.log('hi');},
-
 		insertWordInFireBase : function(ref,id,word){
-			var wordRef = ref.child('IDs').child(id).child('words').push();
-			wordRef.set(word);
+			var data;
+			var bool = false;	
+			ref.child('IDs').child(id).child('words').on('value',function(snap){
+				data= snap.val();
+				for(var key in data){
+					if(data[key] === word){
+						bool = true;
+						break;
+					}
+				}
+				if(!bool){
+					var wordRef = ref.child('IDs').child(id).child('words').push();
+					wordRef.set(word.toLowerCase());
+				}
+			});
 		},
 
 		insertRandom : function(ref){
+			
 			id = Math.random();
 			var randomRef = ref.push();
 			ref.set(id);
